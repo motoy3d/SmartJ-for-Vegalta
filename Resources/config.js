8d7a037aef222abe2898bf06dffa14b48822a286
motoy3d <motoy3d@gmail.com>
2014-12-15 04:13:56 +0900

    first commit

---
+++ Resources/config.js
@@ -0,0 +1,27 @@
+exports.config = {
+    teamId: 'vegalta'
+    ,hashtag: 'vegalta'
+    ,teamName: '仙台'
+    ,appName: 'スマートJ for ベガルタ仙台'
+    ,iPhoneAppUrl: 'https://itunes.apple.com/jp/app/id926225746?l=ja&ls=1&mt=8'
+    ,androidAppUrl: 'https://play.google.com/store/apps/details?id=smartj.vegalta'
+    ,color: '#fcc602'
+    ,tintColor: 'white'
+    ,dbName: 'smartj.vegalta'
+    //ニュースのソースURL
+    ,feedUrlBase: 'http://sub0000499082.hmk-temp.com/redsmylife/news.json'
+    //日程・結果のソースURL
+    ,resultsUrl: "http://sub0000499082.hmk-temp.com/redsmylife/results.json?teamId=vegalta&season="
+    //設定、メッセージのソースURL
+    ,messageUrl: "http://sub0000499082.hmk-temp.com/redsmylife/config.json?teamId=vegalta"
+    ,newsEntriesPerPage: 50
+    ,googleAnalyticsTrackingId: 'UA-30928840-1'
+    //nend(iPhone)
+    ,nendSpotIdIPhoneIcon: 244697
+    ,nendApiKeyIPhoneIcon: '8c74aa8baa42c443597d790cab5a312e6286e82c'
+    ,nendSpotIdIPhoneBanner: 244696
+    ,nendApiKeyIPhoneBanner: '12462049d159b0b16ae0ddc521b59d7a87547d0a'
+    //nend(Android)
+    ,nendSpotIdAndroid: 244695
+    ,nendApiKeyAndroid: '70a1832129c40692f5a1add1dfac92070dd6da7a'
+};
