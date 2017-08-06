8c1720cd402ce63ab4dbf408ce64c18c341c5547
motoy3d <motoy3d@gmail.com>
2017-08-06 23:09:58 +0900

    iOS 1.0.2, Android 1.0.1 バグ修正。ATS有効化、キャッシュクリア、nend削除。

--- Resources/config.js
+++ Resources/config.js
@@ -2,18 +2,40 @@ exports.config = {
     teamId: 'vegalta'
     ,hashtag: 'vegalta'
     ,teamName: '仙台'
+    ,teamNameFull: 'ベガルタ仙台'
     ,appName: 'スマートJ for ベガルタ仙台'
     ,iPhoneAppUrl: 'https://itunes.apple.com/jp/app/id926225746?l=ja&ls=1&mt=8'
     ,androidAppUrl: 'https://play.google.com/store/apps/details?id=smartj.vegalta'
+    ,backgroundColor: 'black'
+	,visitedBgColor : '#457'
+    ,mainTextColor: 'white'
     ,color: '#fcc602'
-    ,tintColor: 'white'
+    ,tintColor: '#254695'
+    ,standingsBgcolor: '#fcc602'
+    ,standingsTeamFontColor: 'black'
+    ,resultsDetailBtnBgColorActive: '#333'
+    ,resultsDetailBtnColorActive: 'white'
+    ,resultsDetailBtnBgColorInactive: '#202020'
+    ,resultsDetailBtnColorInactive: '#444'
     ,dbName: 'smartj.vegalta'
+    //URLベース
+    ,urlBase: 'https://smartj.mobi/redsmylife/'
     //ニュースのソースURL
-    ,feedUrlBase: 'http://sub0000499082.hmk-temp.com/redsmylife/news.json'
+    ,feedUrlBase: 'https://smartj.mobi/redsmylife/news.json'
     //日程・結果のソースURL
-    ,resultsUrl: "http://sub0000499082.hmk-temp.com/redsmylife/results.json?teamId=vegalta&season="
+    ,resultsUrl: "https://smartj.mobi/redsmylife/results.json?teamId=vegalta&season="
+    //順位表のソースURL
+    ,standingsUrl: 'https://smartj.mobi/redsmylife/standings.json'
     //設定、メッセージのソースURL
-    ,messageUrl: "http://sub0000499082.hmk-temp.com/redsmylife/config.json?teamId=vegalta"
+    ,messageUrl: "https://smartj.mobi/redsmylife/config.json?teamId=vegalta"
+    //動画のソースURL
+    ,videoUrl: "https://smartj.mobi/redsmylife/video.json?teamId=vegalta"
+    //報告URL
+    ,reportUrl: "https://smartj.mobi/redsmylife/report.json?teamId=vegalta"
+    //利用規約URL
+    ,rulesUrl: "https://smartj.mobi/redsmylife/rules.jsp?appName="
+    //開発元メールアドレス
+    ,developerMail: "motoy3d+smartj@gmail.com"
     ,newsEntriesPerPage: 50
     ,googleAnalyticsTrackingId: 'UA-30928840-1'
     //nend(iPhone)
